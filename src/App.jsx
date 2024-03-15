c39dbadf90c826aedda2ba8be66b0829f1ce0612
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-15 19:58:03 -0400

    feat: fetch meal data

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { MealList } from './components/MealList';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  const handleChange = e => {
+    setCalories(e.target.value);
+  }
+
+  const getMealData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/mealplanner/generate?apiKey=7ee5a525aaef40808ebc28627d337369&timeFrame=day&targetCalories${calories}`
+      );
+      const data = await response.json();
+      setMealData(data);
+    } catch (error) {
+      console.log("Error");
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <section className="controls">
+        <input type="number" placeholder='Calorias (e.g. 2000)' onChange={handleChange} />
+      </section>
+      <button onClick={getMealData}>Obtener Plan Diario</button>
+      {mealData && <MealList mealList={mealData} />}
+    </div>
   )
 }
 
---
+++ src/Components/MealList.jsx
@@ -0,0 +1,5 @@
+export const MealList = () => {
+  return (
+    <div>MealList</div>
+  )
+}
