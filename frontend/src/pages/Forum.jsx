c6e0422fb2b828b65c75504d0343765cfa10be19
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-03-27 15:24:35 -0400

    feat: add forum component

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -1,7 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from '../components/ChatFeed/ChatFeed';
 import './CSS/Forum.css';
 
 export const Forum = () => {
   return (
-    <div>Forum</div>
-  )
+    <div className='forum'>
+      <ChatEngine
+        height="100vh"
+        projectID="b7be444f-a2a6-41e1-adb2-3ef97a10144c"
+        userName="admin"
+        userSecret="12345"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
+    </div>
+  );
 }
