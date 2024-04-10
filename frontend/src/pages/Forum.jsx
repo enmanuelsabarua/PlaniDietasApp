47128e8d097d401b5303bf8b8911e23b88ad295d
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-04-10 18:24:03 -0400

    fix: refactor ChatFeed component to remove chat subtitle

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -3,14 +3,17 @@ import ChatFeed from '../components/ChatFeed/ChatFeed';
 import './CSS/Forum.css';
 
 export const Forum = () => {
+  const loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+
+  console.log(loggedUser);
 
   return (
     <div className='forum'>
       <ChatEngine
         height="90vh"
         projectID="3a2c7ff7-d120-4899-a273-907962b4b4e0"
-        userName="abarua"
-        userSecret="UD.NZqUP6t@#u-b"
+        userName={loggedUser.email}
+        userSecret={loggedUser.email}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       />
     </div>
